58eb18804c4ad533fa3b3bae260cfed3f71eb388
Bijan Regmi <vizandabijan@gmail.com>
2021-11-02 14:31:05 +0545

    Game loop updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,5 @@
-#include "grid.hpp"
+#include "Game.hpp"
 
 int main(){
-    const int length = 100;
-    const int g_X = 5;
-    const int g_Y = 4;
-    
-    sf::Event ev;
-    sf::RenderWindow win(sf::VideoMode(500, 500), "GRID");
-    win.setFramerateLimit(60);
-    
-    grid g(g_X, g_Y, &win);
-
-    while(win.isOpen()){
-        while(win.pollEvent(ev)){
-            if (ev.type == sf::Event::KeyPressed && ev.key.code == sf::Keyboard::Q)
-                win.close();
-            if (ev.type == sf::Event::MouseButtonPressed){
-                g.handleinput(ev);
-            }
-        }
-        g.render();
-    }
+    Game(500, 600, "Chain Rxn");
 }
