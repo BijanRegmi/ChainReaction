2651cab572443691f4a008ec2bb2c081a6fd5fd5
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2021-11-02 14:31:05 +0545

    Game loop updated

---
+++ include/Game.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "SceneHandler.hpp"
+#include "ResourceManager.hpp"
+
+#include <SFML/Graphics.hpp>
+
+struct GameData{
+    SceneHandler handler;
+    ResourceManager res;
+    sf::RenderWindow window;
+    sf::Event event;
+};
+
+typedef std::shared_ptr<GameData> GameDataRef;
+
+class Game{
+public:
+    Game(int width, int height, std::string title);
+private:
+    GameDataRef _data = std::make_shared<GameData>();
+    
+    void Run();
+};
---
+++ src/Game.cpp
@@ -0,0 +1,18 @@
+#include "Game.hpp"
+#include "SplashScene.hpp"
+
+Game::Game(int width, int height, std::string title){
+    _data->window.create(sf::VideoMode(width, height), title);
+    _data->window.setFramerateLimit(60);
+    _data->handler.AddScene(SceneRef(new SplashScene(_data)));
+    Run();
+}
+
+void Game::Run(){
+    while(_data->window.isOpen()){
+        _data->handler.ProcessSceneChanges();
+        _data->handler.GetActiveScene()->HandleInput();
+        _data->handler.GetActiveScene()->Update();
+        _data->handler.GetActiveScene()->Draw();
+    }
+}
