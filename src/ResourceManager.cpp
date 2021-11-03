c6b3f2e3df566f11b03115240ae9c86af4c6d6f6
Bijan Regmi <vizandabijan@gmail.com>
2021-11-03 12:04:44 +0545

    Sound Loader

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,4 +1,5 @@
 #include "ResourceManager.hpp"
+#include <iostream>
 
 void ResourceManager::LoadTexture(std::string name, std::string fileName){
     sf::Texture tex;
@@ -23,4 +24,15 @@ sf::Font& ResourceManager::GetFont(std::string name){
 void ResourceManager::changeOrigin(sf::Text& text){
     sf::FloatRect textrect = text.getLocalBounds();
     text.setOrigin(textrect.left+textrect.width/2, textrect.top+textrect.height/2);
+}
+
+void ResourceManager::LoadSound(std::string name, std::string fileName){
+    sf::SoundBuffer buff;
+
+    if (buff.loadFromFile(fileName))
+        _buffers[name]=buff;
+}
+
+sf::SoundBuffer& ResourceManager::GetSound(std::string name){
+    return _buffers[name];
 }
